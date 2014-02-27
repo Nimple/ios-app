9b00c808dbd8729002c13335e21511b6dd1b522e
GuidoSchmidt <guidoschmidt@onlinehome.de>
2014-02-27 23:28:13 +0100

    added icons, setup main views & view controllers

---
+++ nimple-iOS/EditNimpleCodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditNimpleCodeViewController.h
+//  nimple-iOS
+//
+//  Created by Guido Schmidt on 27.02.14.
+//  Copyright (c) 2014 nimple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditNimpleCodeViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
---
+++ nimple-iOS/EditNimpleCodeViewController.m
@@ -0,0 +1,61 @@
+//
+//  EditNimpleCodeViewController.m
+//  nimple-iOS
+//
+//  Created by Guido Schmidt on 27.02.14.
+//  Copyright (c) 2014 nimple. All rights reserved.
+//
+
+#import "EditNimpleCodeViewController.h"
+
+@interface EditNimpleCodeViewController ()
+
+@end
+
+@implementation EditNimpleCodeViewController
+{
+    NSArray *fields;
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    fields = [NSArray arrayWithObjects:@"Vorname", @"Nachname", nil];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [fields count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [fields objectAtIndex:indexPath.row];
+    return cell;
+}
+
+@end
