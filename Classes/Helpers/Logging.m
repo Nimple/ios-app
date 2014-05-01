bdb7c0415fd98eba8d7aa202fb45edbd35b450df
Ben John <john@stocard.de>
2014-05-01 21:08:36 +0200

    Restored mixpanel-dev token.

--- Classes/Helpers/Logging.m
+++ Classes/Helpers/Logging.m
@@ -10,10 +10,10 @@
 #import "NimpleAppDelegate.h"
 
 // dev token
-//#define MIXPANEL_TOKEN @"6e3eeca24e9b2372e8582b381295ca0c"
+#define MIXPANEL_TOKEN @"6e3eeca24e9b2372e8582b381295ca0c"
 
 // prod token
-#define MIXPANEL_TOKEN @"c0d8c866df9c197644c6087495151c7e"
+//#define MIXPANEL_TOKEN @"c0d8c866df9c197644c6087495151c7e"
 
 // hash of flyer contact
 #define FLYER_CONTACT_HASH @"9d2b064c3d89c867916c5329f079fa66"
