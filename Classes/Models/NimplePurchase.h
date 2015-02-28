d94379a97b4caf51783771fbddc80c75fe2f9b5b
Ben John <john@stocard.de>
2015-02-28 18:16:44 +0100

    Added nimple purchase draft.

---
+++ Classes/Models/NimplePurchase.h
@@ -0,0 +1,15 @@
+//
+//  NimplePurchase.h
+//  nimple-iOS
+//
+//  Created by Ben John on 28/02/15.
+//  Copyright (c) 2015 nimple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NimplePurchase : NSObject
+
++ (BOOL)isPurchased;
+
+@end
