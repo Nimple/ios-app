9342eab9b7de7368b43b6580065de50bb3ecf840
Ben John <john@stocard.de>
2015-03-09 16:19:23 +0100

    Release build.

--- Classes/AppDelegate/NimpleAppDelegate.m
+++ Classes/AppDelegate/NimpleAppDelegate.m
@@ -15,6 +15,8 @@ colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 \
 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
+#import <sys/types.h>
+#import <sys/sysctl.h>
 #import "NimpleAppDelegate.h"
 #import "NimpleCode.h"
 #import "NimpleModel.h"
